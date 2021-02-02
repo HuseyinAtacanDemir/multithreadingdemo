8fafa413598d64fdf23d1827d41758a474bda17e
HuseyinAtacanDemir <h.atacandemir@gmail.com>
2021-02-02 11:53:51 +0300

    running bash commands on different threads

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
-﻿using System.Threading;
+﻿/* arguments: start program.exe numberOfThreads command commandParameters...
+ */
 using System;
+using System.Threading;
+using System.Diagnostics;
 
 namespace threads
 {
@@ -7,64 +10,96 @@ namespace threads
     {
         public static int threadNo = 0;
     }
-
-    static class Foo
+    public static class Bash
     {
-        public static void bar(int input)
+        public static string run(string cmd)
         {
-            Console.WriteLine("foo" + input + " Started using " + Thread.CurrentThread.Name);
+            var escapedArgs = cmd.Replace("\"", "\\\"");
 
-            int i = 0;
-
-            while (true)
+            var process = new Process()
             {
-                Console.WriteLine("foo" + input + " :" + i++);
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "/bin/bash",
+                    Arguments = $"-c \"{escapedArgs}\"",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
 
-                Thread.Sleep(500);
+            process.Start();
 
-                if (Global.threadNo == input || Global.threadNo == 11)
+            string result = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+
+            return result;
+        }
+    }
+    static class Threadify
+    {
+        public static void threadProcess(string[] command, int input)
+        {
+            Console.WriteLine("Thread" + input + " Started using " + Thread.CurrentThread.Name);
+
+            string commandString = "";
+
+            foreach(string s in command)
+                commandString += s + " ";
+            
+            Console.WriteLine(Thread.CurrentThread.Name + " started command: " + commandString);
+
+            while (true){
+                
+                Console.WriteLine(Thread.CurrentThread.Name + ": " + Bash.run(commandString));
+                Thread.Sleep(1000);
+
+                if (Global.threadNo == input || Global.threadNo == -1)
                     break;
             }
 
-            Console.WriteLine("foo" + input + " Ended using " + Thread.CurrentThread.Name);
+            Console.WriteLine(Thread.CurrentThread.Name + " ended processing command: " + commandString);
 
         }
     }
-
     class Program
     {
-        
         static void Main(string[] args)
         {
-            Console.WriteLine("MultiThreading Demo App");
-            Console.WriteLine("Type the thread number (1-10) to kill a thread");
-            Console.WriteLine("Type 11 to kill all threads or exit.");
-
-
-            Thread[] threads = new Thread[10];
-
-            for(int i = 0; i < 10; i++)
+            if (args.Length > 0)
             {
-                int capture = i;
-                threads[i] = new Thread(() => Foo.bar(capture+1))
+                Console.WriteLine("Type the thread number (1-10) to kill a thread");
+                Console.WriteLine("Type -1 to kill all threads or exit.");
+
+                int threadCount = Convert.ToInt32(args[0]);
+                int paramCount = args.Length;
+
+                string[] command = new string[paramCount - 1];
+                Array.Copy(args, 1, command, 0, paramCount - 1);
+
+                Thread[] threads = new Thread[threadCount];
+
+                for (int i = 0; i < threadCount; i++)
                 {
-                    Name = "Thread" + (capture+1) + ""
-                };
-                threads[i].Start();
-            }
-
-            while (true)
-            {
-
-                Global.threadNo = Convert.ToInt32(Console.ReadLine());
-
-                if (Global.threadNo == 11)
-                {
-                    break;
+                    int capture = i;
+                    threads[i] = new Thread(() => Threadify.threadProcess(command, capture + 1)) { Name = "Thread" + (capture + 1) + "" };
+                    threads[i].Start();
                 }
+
+                while (true)
+                {
+                    Global.threadNo = Convert.ToInt32(Console.ReadLine());
+
+                    if (Global.threadNo == -1)
+                        break;
+                }
+
+                Console.WriteLine("Main Thread Ended, all Child Threads Ended");
             }
-            Console.WriteLine("Main Thread Ended, all Child Threads Ended");
-            
+            else
+            {
+                Console.WriteLine("Please specify arguments");
+            }   
         }
     }
 }
